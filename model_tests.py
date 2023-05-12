941fba493b67054d057cb846aae6b965099d3db0
08Miguel24 <MichaelAveuc571@gmail.com>
2023-05-12 19:19:47 +0800

    saved model to h5 file

--- model_tests.py
+++ model_tests.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from baseline_model_train import X_tests, Y_tests
 
 if __name__ == "__main__":
-    baseline_model = tf.keras.models.load_model('./models/baseline_model.h5')
+    baseline_model = tf.keras.saving.load_model('./models/baseline_model.h5')
     baseline_model.summary()
     # baseline.evaluate()
     
