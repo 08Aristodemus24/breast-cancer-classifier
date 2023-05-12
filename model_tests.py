229e51b56075a2f4cd7994c35bb2dcf5197b59c1
08Miguel24 <MichaelAveuc571@gmail.com>
2023-05-12 20:59:41 +0800

    evaluated baseline with test set

--- model_tests.py
+++ model_tests.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
+import numpy as np
 from baseline_model_train import X_tests, Y_tests
 
 if __name__ == "__main__":
-    baseline_model = tf.keras.saving.load_model('./models/baseline_model.h5')
-    baseline_model.summary()
-    # baseline.evaluate()
+    baseline_model = tf.keras.models.load_model('./models/baseline_model.h5')
+    # baseline_model.summary()
+    results = baseline_model.evaluate(X_tests, Y_tests)
+    print(results)
+
+    logits = baseline_model.predict(X_tests)
+    Y_preds = (tf.nn.sigmoid(logits).numpy() >= 0.5).astype(int)
+
+    print(Y_preds)
+
+
     
